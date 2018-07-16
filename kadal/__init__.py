24902963353f587701db5c6f5a3e8080958e07b5
SynderBlack <frogermanz@gmail.com>
2018-07-17 02:11:35 +0300

    add Klient alias to Client

--- kadal/__init__.py
+++ kadal/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa
 
-from kadal.client import Client, KadalError, MediaNotFound
+from kadal.client import Client, Client as Klient, KadalError, MediaNotFound
 from kadal.media import MediaFormat, MediaStatus, MediaType, Media
