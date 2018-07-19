5ea55cab99acc6f17129e65add714e40fac6e1c1
SynderBlack <frogermanz@gmail.com>
2018-07-20 02:18:38 +0300

    remove debug print

--- kadal/user.py
+++ kadal/user.py
@@ -4,7 +4,6 @@ from kadal.utils import camel2snake
 class User:
     def __init__(self, json):
         d = json['data']['User']
-        print(d)
         if d.get('avatar'):
             d['avatar'] = d['avatar']['large']
         for k, v in d.items():
