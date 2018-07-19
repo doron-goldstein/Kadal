3e46a28275216e9767c7f71c4a371a32b4d51173
SynderBlack <frogermanz@gmail.com>
2018-07-20 02:16:05 +0300

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements = []
 setup(
     name='Kadal',
     author='SynderBlack',
-    version="0.1.5",
+    version="0.2",
     packages=['kadal'],
     license='MIT',
     description='Async wrapper for the Anilist API',
